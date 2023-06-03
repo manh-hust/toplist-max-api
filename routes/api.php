f5883d4ac6adcf5cdb4fae5649f5aa94b646e71e
btcs-manhdd <manhdd@btc-studio.io>
2023-06-03 17:38:47 +0700

    check data db

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Helpers\ApiResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get("/massage_places", function () {
+    $massagePlaces = App\Models\MassagePlace::all();
+    return ApiResponse::createSuccessResponse($massagePlaces);
 });
